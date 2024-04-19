40568821d25bf3fe30dfb958b85e9b378de6a6b4
Michael Redig <mredig@gmail.com>
2024-04-18 23:26:50 -0500

    (refactor) cleaner backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,39 +1,54 @@
 #!/usr/bin/env bash
 
-BORG_DIR="/borgdir/backup_repo"
+## imported env vars
+# '?' indicates optional
+# RCLONE_S3_ACCESS_KEY_ID
+# RCLONE_S3_SECRET_ACCESS_KEY
+#   OR
+# RCLONE_S3_ACCESS_KEY_ID_FILE
+# RCLONE_S3_SECRET_ACCESS_KEY_FILE
+#
+# BACKUP_TITLE
+# KEEP_STATS?
+# RCLONE_REMOTE_NAME
+# RCLONE_REMOTE_BACKUP_PATH
+## end
 
-if [ ! -d "${BORG_DIR}" ]; then
-    borg init -e none "${BORG_DIR}"
-    echo "Created repo at ${BORG_DIR}"
+BORG_REPO_PARENT_DIR="/borgdir"
+BORG_REPO_DIR="${BORG_REPO_PARENT_DIR}/backup_repo"
+BORG_BACKUPS_DIR="/backups"
+RCLONE_CONF_FILE_PATH="/rclone.conf"
+
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
+
+if [ ! -d "${BORG_REPO_DIR}" ]; then
+    borg init -e none "${BORG_REPO_DIR}"
+    echo "Created repo at ${BORG_REPO_DIR}"
 else
-    echo "Using existing repo at ${BORG_DIR}"
+    echo "Using existing repo at ${BORG_REPO_DIR}"
 fi
 
-# BACKUP_TIMESTAMP=$(date -Iseconds)
-BACKUP_TIMESTAMP=$(date +%s)
-
-THIS_BACKUP_TITLE="${BACKUP_TITLE}-${BACKUP_TIMESTAMP}"
+THIS_BACKUP_TITLE="${BACKUP_TITLE}-{fqdn}_{now}"
 
 COMMAND=("borg")
 COMMAND+=("create")
-if [[ $KEEP_STATS ]]; then
-    COMMAND+=("--stats")
-fi
-COMMAND+=("${BORG_DIR}::${THIS_BACKUP_TITLE}")
-COMMAND+=("/backups")
+COMMAND+=("--stats")
+COMMAND+=("${BORG_REPO_DIR}::${THIS_BACKUP_TITLE}")
+COMMAND+=("${BORG_BACKUPS_DIR}")
 
-if [[ $KEEP_STATS ]]; then
-    "${COMMAND[@]}" >> /root/stats
-    echo "" >> /root/stats
-else
-    "${COMMAND[@]}"
-fi
+# if [[ $KEEP_STATS ]]; then
+#     "${COMMAND[@]}" >> /root/stats
+#     echo "" >> /root/stats
+# else
+# fi
+"${COMMAND[@]}"
 
 if [[ -z "$RCLONE_S3_ACCESS_KEY_ID" ]]; then
     echo "RCLONE_S3_ACCESS_KEY_ID undefined. Checking for file."
     if [[ "$RCLONE_S3_ACCESS_KEY_ID_FILE" ]]; then
         echo "RCLONE_S3_ACCESS_KEY_ID_FILE defined. Loading."
-        RCLONE_S3_ACCESS_KEY_ID=$(cat "${RCLONE_S3_ACCESS_KEY_ID_FILE}")
+        export RCLONE_S3_ACCESS_KEY_ID=$(cat "${RCLONE_S3_ACCESS_KEY_ID_FILE}")
     else
         echo "No key provided for RCLONE_S3_ACCESS_KEY_ID - aborting rclone upload".
         exit 1
@@ -44,25 +59,27 @@ if [[ -z "$RCLONE_S3_SECRET_ACCESS_KEY" ]]; then
     echo "RCLONE_S3_SECRET_ACCESS_KEY undefined. Checking for file."
     if [[ "$RCLONE_S3_SECRET_ACCESS_KEY_FILE" ]]; then
         echo "RCLONE_S3_SECRET_ACCESS_KEY_FILE defined. Loading."
-        RCLONE_S3_SECRET_ACCESS_KEY=$(cat "${RCLONE_S3_SECRET_ACCESS_KEY_FILE}")
+        export RCLONE_S3_SECRET_ACCESS_KEY=$(cat "${RCLONE_S3_SECRET_ACCESS_KEY_FILE}")
     else
         echo "No key provided for RCLONE_S3_ACCESS_KEY_ID - aborting rclone upload".
         exit 1
     fi
 fi
 
-if [[ ! -r /rclone.conf ]]; then
+if [[ ! -e "${RCLONE_CONF_FILE_PATH}" ]]; then
     echo "No rclone config file at /rclone.conf! aborting"
     exit 1
 fi
 
+export RCLONE_S3_NO_CHECK_BUCKET=true
+
 RCOMMAND=("rclone")
 RCOMMAND+=("-P")
-RCOMMAND+=("--config=/rclone.conf")
+RCOMMAND+=("--config=${RCLONE_CONF_FILE_PATH}")
 RCOMMAND+=("sync")
-RCOMMAND+=("/borgdir/")
+RCOMMAND+=("${BORG_REPO_PARENT_DIR}/")
 RCOMMAND+=("${RCLONE_REMOTE_NAME}:${RCLONE_REMOTE_BACKUP_PATH}/${BACKUP_TITLE}/")
 
-echo "Running '${COMMAND[@]}'"
+echo "Running '${RCOMMAND[@]}'"
 
 "${RCOMMAND[@]}"
